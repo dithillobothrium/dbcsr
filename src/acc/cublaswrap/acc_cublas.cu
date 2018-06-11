d4540c1cbad8099ed3c317690a27f4c2d7060385
Ilia Sivkov <isivkov@tcgpu2.chem.uzh.ch>
2018-06-11 16:17:52 +0200

    cublas batched + looped is done

--- src/acc/cublaswrap/acc_cublas.cu
+++ src/acc/cublaswrap/acc_cublas.cu
@@ -6,68 +6,130 @@
 
 #ifdef __ACC_CUBLAS
 
-extern "C" int acc_dgemm (cublasHandle_t *handle, int m, int n, int k, int transa, int transb, 
-                          void *a_data, int a_lb, void *b_data, int b_lb, void *c_data, int c_lb, 
-			  double alpha, double beta, void *stream)
+#include <vector>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+extern "C" int acc_dgemm(cublasHandle_t *handle, void *stack_params, int stack_size, int mnk, int transa, int transb, 
+                          void *a_data, void *b_data, void *c_data, double alpha, double beta, void *stream)
 {
-  cudaStream_t* custream = (cudaStream_t*) stream;
-  double *a = (double*)a_data;
-  double *b = (double*)b_data;
-  double *c = (double*)c_data;
-/*  
-  double *host;
-  int sz = m*k > n*k ? n*k : m*k;
-  int print_sz = 50;
-
-  host = (double*)malloc(sizeof(double)*sz);
-  
-  cudaMemcpy(host,a,sizeof(double)*sz,cudaMemcpyDeviceToHost);
-  for (int i=0; i < (print_sz < sz ? print_sz: sz) ; i++) printf("%f ",host[i]);
-  printf("\n");
-   
-  cudaMemcpy(host,b,sizeof(double)*sz,cudaMemcpyDeviceToHost);
-  for (int i=0; i < (print_sz < sz ? print_sz: sz) ; i++) printf("%f ",host[i]);
-  printf("\n");
-  
-  free(host);
-
-*/  
-   // Check transposity
-  char transa_char = 'N';
-  cublasOperation_t trana = CUBLAS_OP_N;
-  int lda = m;
-
-  if (transa!=0) {
-    transa_char = 'T';
-    trana = CUBLAS_OP_T;
-    lda = k;
-  }
-
-  char transb_char = 'N';
-  cublasOperation_t tranb = CUBLAS_OP_N;
-  int ldb = k;
-
-  if (transb!=0) {
-    transb_char = 'T';
-    tranb = CUBLAS_OP_T;
-    ldb = n;
-  }
- 
-//  printf("in C-cublas, transa = %c transb = %c \n",transa_char,transb_char);
-
-//  printf("m n k = %d %d %d, alpha beta = %f %f, a_lb b_lb c_lb = %d %d %d\n",m,n,k,alpha,beta,a_lb, b_lb, c_lb);
-
-  cublasSetStream(*handle, *custream);  
-   
-  cublasStatus_t stat = cublasDgemm(*handle, trana, tranb, m, n, k, &alpha, &a[a_lb], lda, &b[b_lb], ldb, &beta, &c[c_lb], m);
+    cudaStream_t* custream = (cudaStream_t*) stream;
+    double *a = (double*)a_data;
+    double *b = (double*)b_data;
+    double *c = (double*)c_data;
+    int *stack = (int*)stack_params;
     
-  if (stat != CUBLAS_STATUS_SUCCESS) return(-1);
+    // TODO: something smarter
+    int stack_width = 7;
+ 
+    /*  
+    double *host;
+    int sz = m*k > n*k ? n*k : m*k;
+    int print_sz = 50;
+ 
+    host = (double*)malloc(sizeof(double)*sz);
+    
+    cudaMemcpy(host,a,sizeof(double)*sz,cudaMemcpyDeviceToHost);
+    for (int i=0; i < (print_sz < sz ? print_sz: sz) ; i++) printf("%f ",host[i]);
+    printf("\n");
+     
+    cudaMemcpy(host,b,sizeof(double)*sz,cudaMemcpyDeviceToHost);
+    for (int i=0; i < (print_sz < sz ? print_sz: sz) ; i++) printf("%f ",host[i]);
+    printf("\n");
+    
+    free(host);
+ 
+    */
+    
+      
+  
+    printf("in C-cublas");
+ 
+ 
+    cublasSetStream(*handle, *custream);  
+  
+    auto check_transposity = [] (int transa, int transb, int m, int n, int k, cublasOperation_t& trana, cublasOperation_t& tranb, int& lda, int& ldb)
+    {
+        trana = CUBLAS_OP_N;
+        lda = m;
+    
+        if (transa!=0) {
+          trana = CUBLAS_OP_T;
+          lda = k;
+        }
+    
+        tranb = CUBLAS_OP_N;
+        ldb = k;
+    
+        if (transb!=0) {
+          tranb = CUBLAS_OP_T;
+          ldb = n;
+        }
+    };
 
+
+    int lda, ldb;
+    cublasOperation_t trana, tranb;
+
+    if (mnk == 1)
+    {
+        int m = stack[ 0 ];
+        int n = stack[ 1 ];
+        int k = stack[ 2 ];
+         
+        thrust::host_vector<double*> a_ptrs_h(stack_size), b_ptrs_h(stack_size), c_ptrs_h(stack_size);
+ 
+        for (int i = 0; i < stack_size; i++)
+        {
+            a_ptrs_h[i] = &a[ stack[ stack_width * i + 3 ] - 1 ];
+            b_ptrs_h[i] = &b[ stack[ stack_width * i + 4 ] - 1 ];
+            c_ptrs_h[i] = &c[ stack[ stack_width * i + 5 ] - 1 ];
+        }
+
+        thrust::device_vector<double*> a_ptrs, b_ptrs, c_ptrs;
+
+        a_ptrs = a_ptrs_h;
+        b_ptrs = b_ptrs_h;
+        c_ptrs = c_ptrs_h;
+
+        check_transposity(transa, transb, m, n, k, trana, tranb, lda, ldb);
+       
+        printf("batched");
+
+        cublasStatus_t stat = cublasDgemmBatched(*handle, trana, tranb, m, n, k, &alpha, (const double**)thrust::raw_pointer_cast(a_ptrs.data()), lda, 
+                                                 (const double**)thrust::raw_pointer_cast(b_ptrs.data()), ldb, &beta, (double**)thrust::raw_pointer_cast(c_ptrs.data()), m, stack_size);
+       
+        if (stat != CUBLAS_STATUS_SUCCESS) return(-1);
+        
+        return 0;
+    }
+    
+    
+    for (int i = 0; i < stack_size; i++)
+    {
+        // get mnk from stack data
+        int m = stack[ stack_width * i ];
+        int n = stack[ stack_width * i + 1];
+        int k = stack[ stack_width * i + 2];
+ 
+        // get first element of data, index - 1 becasue data comes from fortran
+        double * a_buf = &a[ stack[ stack_width * i + 3 ] - 1 ];
+        double * b_buf = &b[ stack[ stack_width * i + 4 ] - 1];
+        double * c_buf = &c[ stack[ stack_width * i + 5 ] - 1 ];
+ 
+        check_transposity(transa, transb, m, n, k, trana, tranb, lda, ldb);
+        
+        //printf("m n k = %d %d %d, alpha beta = %f %f, a_lb b_lb c_lb = %d %d %d\n",m,n,k,alpha,beta,stack[ stack_width * i + 3], stack[ stack_width * i + 4 ], stack[ stack_width * i + 5 ]);
+        cublasStatus_t stat = cublasDgemm(*handle, trana, tranb, m, n, k, &alpha, a_buf, lda, b_buf, ldb, &beta, c_buf, m);
+ 
+        if (stat != CUBLAS_STATUS_SUCCESS) return(-1);
+    }
 //  cudaDeviceSynchronize();
 
 //  printf("cublas OK");
   return(0); 
 }
+
 
 
 // cublas interface ------------------
